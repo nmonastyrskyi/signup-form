e410de165a7a6098831852eadd389aad014a8d01
Nikita Monastyrskyi <nikita.monastyrskyi@brightpattern.com>
2024-12-14 00:03:52 +0100

    Supress eslint error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
+//eslint-disable-next-line import/no-default-export
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 });
